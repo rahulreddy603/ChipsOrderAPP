869bcad703faf065f0e8412c92ca8e20ad06a77e
rahulreddy603 <145222551+rahulreddy603@users.noreply.github.com>
2025-08-15 14:04:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ app.use(cors());
 app.use(express.json({ limit: "10kb" }));
 
 const razorpay = new Razorpay({
-  key_id: process.env.KEY_ID,
-  key_secret: process.env.KEY_SECREATE,
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
 app.post("/api/create-order", async (req, res) => {
@@ -39,7 +39,7 @@ app.post("/api/verify-payment", (req, res) => {
   try {
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
     const generated_signature = crypto
-      .createHmac("sha256", process.env.KEY_SECRATE)
+      .createHmac("sha256", process.RAZORPAY_KEY_SECRET)
       .update(`${razorpay_order_id}|${razorpay_payment_id}`)
       .digest("hex");
 
