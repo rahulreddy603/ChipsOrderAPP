117c81c1c856fe2b6984e492ce160caf09a8a914
rahulreddy603 <145222551+rahulreddy603@users.noreply.github.com>
2025-08-15 13:41:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
+  key_id: rzp_live_KbNeSNLqfHTzAB,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
